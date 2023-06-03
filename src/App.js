303ea3c7eb46547e5be555fa3dd1365e7ccbd6d9
Rob Schuster <robert.is@icloud.com>
2023-06-03 14:16:49 -0400

    Revised text added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Wow, I'm working with React on my machine!</h1>
+      <p>Is this what happiness feels like?</p>
     </div>
   );
 }
