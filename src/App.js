9e5caec02cc35b67d800e7f9187e176563d3a7b8
Rob Schuster <robert.is@icloud.com>
2023-06-03 14:49:44 -0400

    Installed react-confetti and added it to the App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Confetti from "react-confetti";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"><Confetti />
       <h1>Wow, I'm working with React on my machine!</h1>
       <p>Is this what happiness feels like?</p>
     </div>
